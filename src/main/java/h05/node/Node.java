7ea926c6a1a2b097a09eecb1cd5c22c7dde5787d
Nico Schnieders <nico.schnieders@bolindalabs.com>
2025-05-09 10:12:25 +0200

    Add "getting mined" functionality to Nodes

--- src/main/java/h05/node/Node.java
+++ src/main/java/h05/node/Node.java
@@ -1,12 +1,14 @@
 package h05.node;
 
-import fopbot.FieldEntity;
+import fopbot.Block;
+import fopbot.World;
 import h05.Mineable;
+import h05.equipment.Tool;
 
-public abstract class Node extends FieldEntity implements Mineable {
+public abstract class Node extends Block implements Mineable {
     private State miningState;
     private int durability;
-    private String name;
+    private final String name;
 
     public Node(int x, int y, String name) {
         super(x, y);
@@ -27,6 +29,33 @@ public abstract class Node extends FieldEntity implements Mineable {
     public String getName() {
         return name;
     }
+
+    public int getMined(Tool tool) {
+        int amountToReduceDurabilityBy = 0;
+        if (tool == null) {
+            amountToReduceDurabilityBy = 5;
+        } else if
+        (getName().equals("rock") && tool.getName().equals("pickaxe")) {
+            amountToReduceDurabilityBy = 10;
+        } else if (getName().equals("tree") && tool.getName().equals("axe")) {
+            amountToReduceDurabilityBy = 10;
+        }
+        reduceDurability(amountToReduceDurabilityBy);
+        updateAfterMining();
+        return amountToReduceDurabilityBy;
+    }
+
+    public void updateAfterMining() {
+        if (durability < 100 && durability > 50) {
+            setMiningState(State.HALF_MINED);
+        } else if (durability <= 50 && durability > 0) {
+            setMiningState(State.FULLY_MINED);
+        }
+        if (durability <= 0) {
+            World.getGlobalWorld().removeEntity(getX(), getY(), getClass());
+        }
+    }
+
 
     public State getMiningState() {
         return miningState;

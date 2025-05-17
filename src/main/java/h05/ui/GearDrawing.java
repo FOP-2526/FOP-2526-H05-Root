efd3fb25ea57ab96b0188bc3ee9e6ef8ea70cfc7
nyanyan <zentox@users.noreply.github.com>
2025-05-17 19:27:57 +0200

    Fix GearDrawing

--- src/main/java/h05/ui/GearDrawing.java
+++ src/main/java/h05/ui/GearDrawing.java
@@ -6,67 +6,89 @@ import fopbot.SvgBasedDrawing;
 import h05.Equipment;
 import h05.entity.Gear;
 import h05.equipment.Axe;
+import h05.equipment.Battery;
+import h05.equipment.Camera;
 import h05.equipment.Pickaxe;
+import h05.equipment.Powerbank;
+import h05.equipment.TelephotoLens;
+import h05.equipment.Tool;
 import org.tudalgo.algoutils.student.annotation.DoNotTouch;
 
 import java.awt.Image;
+import java.util.List;
 
 @DoNotTouch
 public class GearDrawing extends SvgBasedDrawing<Gear> {
 
+    public static final List<Class<? extends Equipment>> AVAILABLE_EQUIPMENTS = List.of(
+        Battery.class,
+        Camera.class
+        //Powerbank.class,
+        //TelephotoLens.class
+    );
+
+    public static final List<Class<? extends Tool>> AVAILABLE_TOOLS = List.of(
+        Axe.class,
+        Pickaxe.class
+    );
+
+    public static final int EQUIPMENTS_IMAGES = 4;
+
+    public static final int TOOL_IMAGES = 1;
+
     @DoNotTouch
     public GearDrawing() {
-        super(Equipment.Condition.values().length);
+        super(
+            AVAILABLE_EQUIPMENTS.size() * EQUIPMENTS_IMAGES
+                + AVAILABLE_TOOLS.size() * TOOL_IMAGES
+        );
     }
 
     @DoNotTouch
     @Override
     protected Image getCurrentDrawingImage(Gear entity) {
         var equipment = entity.getEquipment();
-        if (equipment instanceof Axe) {
-            return getImage(0);
+        int index;
+        Class<? extends Equipment> clazz = equipment.getClass();
+        if (equipment instanceof Tool) {
+            index = AVAILABLE_EQUIPMENTS.size() * EQUIPMENTS_IMAGES + AVAILABLE_TOOLS.indexOf(clazz) * TOOL_IMAGES;
+        } else {
+            index = AVAILABLE_EQUIPMENTS.indexOf(clazz) * EQUIPMENTS_IMAGES + equipment.getCondition().ordinal();
         }
-        if (equipment instanceof Pickaxe) {
-            return getImage(1);
-        }
-        return getImage(entity.getEquipment().getCondition().ordinal());
+        return getImage(index);
     }
 
     @DoNotTouch
     @Override
     protected void loadImages(int targetSize, DrawingContext<? extends Gear> context) {
         Gear entity = context.entity();
-        Equipment equipment = entity.getEquipment();
         Equipment.Condition[] conditions = Equipment.Condition.values();
-
-        if (equipment instanceof Axe) {
-            String path = equipment.getName() + EXTENSION;
-            Image image = PaintUtils.loadFieldImage(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream(path),
-                0,
-                targetSize
-            );
-            setImage(0, image);
-        } else if (equipment instanceof Pickaxe) {
-            String path = equipment.getName() + EXTENSION;
-            Image image = PaintUtils.loadFieldImage(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream(path),
-                0,
-                targetSize
-            );
-            setImage(1, image);
-        }
-        else {
+        for (Class<? extends Equipment> clazz : AVAILABLE_EQUIPMENTS) {
             for (Equipment.Condition condition : conditions) {
-                String path = equipment.getName() + "_" + condition.name().toLowerCase() + EXTENSION;
-                Image image = PaintUtils.loadFieldImage(
-                    Thread.currentThread().getContextClassLoader().getResourceAsStream(path),
-                    0,
-                    targetSize
-                );
-                setImage(condition.ordinal(), image);
+                String path = clazz.getSimpleName() + "_" + condition.name().toLowerCase() + EXTENSION;
+                try {
+                    Image image = PaintUtils.loadFieldImage(
+                        Thread.currentThread().getContextClassLoader().getResourceAsStream(path),
+                        0,
+                        targetSize
+                    );
+                    setImage(AVAILABLE_EQUIPMENTS.indexOf(clazz) * EQUIPMENTS_IMAGES + condition.ordinal(), image);
+                } catch (Exception e) {
+                    System.err.println(path);
+                    throw e;
+                }
+
             }
         }
-
+        int offset = AVAILABLE_EQUIPMENTS.size() * EQUIPMENTS_IMAGES;
+        for (Class<? extends Tool> clazz : AVAILABLE_TOOLS) {
+            String path = clazz.getSimpleName() + EXTENSION;
+            Image image = PaintUtils.loadFieldImage(
+                Thread.currentThread().getContextClassLoader().getResourceAsStream(path),
+                0,
+                targetSize
+            );
+            setImage(offset + AVAILABLE_TOOLS.indexOf(clazz) * TOOL_IMAGES, image);
+        }
     }
 }

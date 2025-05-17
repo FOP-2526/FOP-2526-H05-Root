0eb850a204389b5484c302fc1b49d1a5783abff1
nyanyan <zentox@users.noreply.github.com>
2025-05-17 20:20:09 +0200

    Move getMined to interface -> onMined

---
+++ src/main/java/h05/AbstractMinableEntity/AbstractMinableEntity.java
@@ -0,0 +1,74 @@
+package h05.AbstractMinableEntity;
+
+import fopbot.Block;
+import fopbot.World;
+import h05.Mineable;
+import h05.equipment.Tool;
+
+public abstract class AbstractMinableEntity extends Block implements Mineable {
+    private State miningState;
+    private int durability;
+    private final String name;
+
+    public AbstractMinableEntity(int x, int y, String name) {
+        super(x, y);
+        this.name = name;
+        this.miningState = State.NOT_MINED;
+        this.durability = 100;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int onMined(Tool tool) {
+        int amountToReduceDurabilityBy = 0;
+        if (tool == null) {
+            amountToReduceDurabilityBy = 5;
+        } else if
+        (getName().equals("rock") && tool.getName().equals("pickaxe")) {
+            amountToReduceDurabilityBy = 20;
+        } else if (getName().equals("tree") && tool.getName().equals("axe")) {
+            amountToReduceDurabilityBy = 20;
+        }
+        reduceDurability(amountToReduceDurabilityBy);
+        updateAfterMining();
+        return amountToReduceDurabilityBy;
+    }
+
+    public void updateAfterMining() {
+        if (durability < 100 && durability > 50) {
+            setMiningState(State.HALF_MINED);
+            World.getGlobalWorld().getGuiPanel().repaint();
+        } else if (durability <= 50 && durability > 0) {
+            setMiningState(State.FULLY_MINED);
+            World.getGlobalWorld().getGuiPanel().repaint();
+        } else if (durability <= 0) {
+            World.getGlobalWorld().removeEntity(getX(), getY(), getClass());
+
+        }
+    }
+
+
+    public State getState() {
+        return miningState;
+    }
+
+    public void setMiningState(State miningState) {
+        this.miningState = miningState;
+    }
+
+    public void reduceDurability(int amount) {
+        durability -= amount;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
+    }
+}

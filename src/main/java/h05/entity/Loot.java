00362c137b2351bcfa76c0c5593ffd160e06a638
nyanyan <zentox@users.noreply.github.com>
2025-06-26 10:26:45 +0200

    Do not exposure gui update for studis

--- src/main/java/h05/entity/Loot.java
+++ src/main/java/h05/entity/Loot.java
@@ -1,8 +1,11 @@
 package h05.entity;
 
 import fopbot.Block;
+import fopbot.World;
+import h05.gear.Tool;
 import h05.loot.Mineable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.tudalgo.algoutils.student.annotation.DoNotTouch;
 
 /**
@@ -29,7 +32,36 @@ public class Loot extends Block {
     @DoNotTouch
     public Loot(int x, int y, @NotNull Mineable mineable) {
         super(x, y);
-        this.mineable = mineable;
+        this.mineable = new Mineable() {
+            @Override
+            public @NotNull String getName() {
+                return mineable.getName();
+            }
+
+            @Override
+            public boolean onMined(@Nullable Tool tool) {
+                boolean isMined = mineable.onMined(tool);
+                if (isMined) {
+                    World.getGlobalWorld().getGuiPanel().updateGui();
+                }
+                return isMined;
+            }
+
+            @Override
+            public int getDurability() {
+                return mineable.getDurability();
+            }
+
+            @Override
+            public void setDurability(int durability) {
+                mineable.setDurability(durability);
+            }
+
+            @Override
+            public void reduceDurability(int amount) {
+                mineable.reduceDurability(amount);
+            }
+        };
     }
 
     /**

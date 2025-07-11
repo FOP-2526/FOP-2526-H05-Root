11c694a397d96affeb575403339058f5a136c509
nschn <ascedance@gmail.com>
2025-07-11 12:04:54 +0200

    Small fixes regarding blocks

--- src/main/java/h05/mineable/Rock.java
+++ src/main/java/h05/mineable/Rock.java
@@ -17,11 +17,11 @@ public class Rock extends AbstractMinableEntity implements Mineable {
     public boolean onMined(@Nullable Tool tool) {
         final double factor;
         if (tool == null) {
-            factor = 0.75;
+            factor = 0.75; // 0.75
         } else if (tool.getName().equals("Pickaxe")) {
-            factor = 3.0;
+            factor = 2; // 2 * 30 = 60
         } else {
-            factor = 1.5;
+            factor = 1.5; // 1.5 * 15 = 22.5
         }
         reduceDurability(tool == null ? 1 * factor : tool.getMiningPower() * factor);
         return getDurability() <= 0;
--- src/main/java/h05/mineable/Tree.java
+++ src/main/java/h05/mineable/Tree.java
@@ -17,11 +17,11 @@ public class Tree extends AbstractMinableEntity implements Mineable {
     public boolean onMined(@Nullable Tool tool) {
         final double factor;
         if (tool == null) {
-            factor = 1;
-        } else if (tool.getName().equals("Pickaxe")) {
-            factor = 4;
+            factor = 1; //1
+        } else if (tool.getName().equals("Axe")) {
+            factor = 4; // 4 * 15 = 60
         } else {
-            factor = 2;
+            factor = 1.5; // 1.5 * 30 = 45
         }
         reduceDurability(tool == null ? 1 * factor : tool.getMiningPower() * factor);
         return getDurability() <= 0;

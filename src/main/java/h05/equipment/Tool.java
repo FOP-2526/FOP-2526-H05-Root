1176f0d288076d2e8c0ad1fbfa6328fb921dcdbf
nschn <ascedance@gmail.com>
2025-07-14 12:38:14 +0200

    Add tool annotations

--- src/main/java/h05/equipment/Tool.java
+++ src/main/java/h05/equipment/Tool.java
@@ -1,10 +1,13 @@
 package h05.equipment;
 
+import org.tudalgo.algoutils.student.annotation.DoNotTouch;
+
 /**
  * A tool represents a piece of equipment that can be used for mining.
  *
  * @author Nhan Huynh, Nico Schnieders
  */
+@DoNotTouch
 public interface Tool extends Equipment {
 
     /**
@@ -12,14 +15,17 @@ public interface Tool extends Equipment {
      *
      * @return the mining power of this tool
      */
+    @DoNotTouch
     double getMiningPower();
 
     @Override
+    @DoNotTouch
     default boolean isUsable() {
         return false;
     }
 
     @Override
+    @DoNotTouch
     default boolean isTool() {
         return true;
     }

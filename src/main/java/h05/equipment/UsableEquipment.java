aa4a7ecf0b4d097cde3e1d31dd2e0e6d202a1de9
nyanyan <zentox@users.noreply.github.com>
2025-07-14 17:51:18 +0200

    Add donottouch annotation

--- src/main/java/h05/equipment/UsableEquipment.java
+++ src/main/java/h05/equipment/UsableEquipment.java
@@ -2,6 +2,7 @@ package h05.equipment;
 
 import h05.entity.Miner;
 import org.jetbrains.annotations.NotNull;
+import org.tudalgo.algoutils.student.annotation.DoNotTouch;
 
 /**
  * Usable equipment is a type of equipment that can be used by a miner to perform actions or gain benefits.
@@ -15,6 +16,7 @@ public interface UsableEquipment extends Equipment {
      *
      * @param miner the miner using this equipment
      */
+    @DoNotTouch
     void use(@NotNull Miner miner);
 
     @Override

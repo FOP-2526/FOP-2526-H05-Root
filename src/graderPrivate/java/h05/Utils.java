201e7877dff6364b09bd40fbdcec85134f3c6691
Vedron2 <98691302+Vedron2@users.noreply.github.com>
2025-08-27 18:15:21 +0200

    Refactor: Remove unused imports

--- src/graderPrivate/java/h05/Utils.java
+++ src/graderPrivate/java/h05/Utils.java
@@ -12,8 +12,6 @@ import java.awt.*;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
